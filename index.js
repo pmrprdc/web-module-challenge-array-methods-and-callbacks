90ee9fbba3f8b23d7b6dc0ebb5cb494a40b73759
Pamir Perdeci <pamirperdeci@gmail.com>
2022-09-06 16:11:57 -0400

    3/6 tests are passing

--- index.js
+++ index.js
@@ -47,9 +47,14 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, cb) {
+    const newArr = cb(array);
+    const years = [];
+    newArr.forEach(x => years.push(x.Year))
+    return years;
 }
+
+console.log(getYears(fifaData, getFinals))
 
 
 
